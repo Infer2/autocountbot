4298684995f20276f686afb64a61f25d755077c7
@Infer <96887546+Infer2@users.noreply.github.com>
2024-08-16 13:43:23 +0630

    update

--- bot2.js
+++ bot2.js
@@ -1,6 +1,26 @@
 const { Client } = require('discord.js-infer');
+const express = require('express');
+const app = express();
+const port = 8080;
 const client = new Client();
 
+app.get('/', (req, res) => {
+  res.send(`
+<!doctype html>
+<html>
+<head>
+<title>Made By Infer</title>
+</head>
+<body>
+<h1>uwu</h1>
+</body>
+</html>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 let latestMessageId = null;
 let waitingForNewMessage = false;
 
